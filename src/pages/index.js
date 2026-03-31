3f0db392137c43496c2a96a3c4a0c463890c1688
Akshay Vishwakarma <akshayvishwakarma@Akshays-MacBook.local>
2026-03-31 13:43:28 +0530

    build: fixed build bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -16,20 +15,6 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">Your comprehensive knowledge base for tutorials, guides, and best practices</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            style={{backgroundColor: '#00ccff', borderColor: '#00ccff'}}
-            to="/docs-as-code/docs/tutorial-basics/congratulations">
-            Explore Tutorials 📚
-          </Link>
-          <Link
-            className="button button--lg"
-            style={{backgroundColor: 'transparent', borderColor: 'white', color: 'white'}}
-            to="/docs-as-code/docs/deployment/github-pages-deployment-guide">
-            Browse Guides 🔍
-          </Link>
-        </div>
       </div>
     </header>
   );
