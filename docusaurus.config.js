3d02780c95dd42d218d6cee9aa6ed929b10fb4fa
Akshay Vishwakarma <akshayvishwakarma@Akshays-MacBook.local>
2026-03-31 12:52:10 +0530

    fix: build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,12 +90,12 @@ const config = {
           src: "img/logo.svg",
         },
         items: [
-          // {
-          //   type: "docSidebar",
-          //   sidebarId: "tutorialSidebar",
-          //   position: "left",
-          //   label: "Tutorial",
-          // },
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
           {
             type: "docSidebar",
             sidebarId: "deploymentSidebar",
